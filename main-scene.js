5a094b93ddc3e9b12de334ba968d9104a3033016
Kent Zhang <kent@Kents-MacBook-Pro.local>
2020-03-01 15:11:06 -0800

    Added assets folder

--- main-scene.js
+++ main-scene.js
@@ -1,5 +1,107 @@
-window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
-    class Assignment_Three_Scene extends Scene_Component {
+export class Shape_From_File extends Shape {
+    // **Shape_From_File** is a versatile standalone Shape that imports
+    // all its arrays' data from an .obj 3D model file.
+    constructor(filename) {
+        super("position", "normal", "texture_coord");
+        // Begin downloading the mesh. Once that completes, return
+        // control to our parse_into_mesh function.
+        this.load_file(filename);
+    }
+
+    load_file(filename) {                             // Request the external file and wait for it to load.
+        // Failure mode:  Loads an empty shape.
+        return fetch(filename)
+            .then(response => {
+                if (response.ok) return Promise.resolve(response.text())
+                else return Promise.reject(response.status)
+            })
+            .then(obj_file_contents => this.parse_into_mesh(obj_file_contents))
+            .catch(error => {
+                this.copy_onto_graphics_card(this.gl);
+            })
+    }
+
+    parse_into_mesh(data) {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];
+        unpacked.norms = [];
+        unpacked.textures = [];
+        unpacked.hashindices = {};
+        unpacked.indices = [];
+        unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;
+        var NORMAL_RE = /^vn\s/;
+        var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;
+        var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            var elements = line.split(WHITESPACE_RE);
+            elements.shift();
+
+            if (VERTEX_RE.test(line)) verts.push.apply(verts, elements);
+            else if (NORMAL_RE.test(line)) vertNormals.push.apply(vertNormals, elements);
+            else if (TEXTURE_RE.test(line)) textures.push.apply(textures, elements);
+            else if (FACE_RE.test(line)) {
+                var quad = false;
+                for (var j = 0, eleLen = elements.length; j < eleLen; j++) {
+                    if (j === 3 && !quad) {
+                        j = 2;
+                        quad = true;
+                    }
+                    if (elements[j] in unpacked.hashindices)
+                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                    else {
+                        var vertex = elements[j].split('/');
+
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                        if (textures.length) {
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 0]);
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 1]);
+                        }
+
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 0]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 1]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 2]);
+
+                        unpacked.hashindices[elements[j]] = unpacked.index;
+                        unpacked.indices.push(unpacked.index);
+                        unpacked.index += 1;
+                    }
+                    if (j === 3 && quad) unpacked.indices.push(unpacked.hashindices[elements[0]]);
+                }
+            }
+        }
+        {
+            const {verts, norms, textures} = unpacked;
+            for (var j = 0; j < verts.length / 3; j++) {
+                this.arrays.position.push(vec3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
+                this.arrays.normal.push(vec3(norms[3 * j], norms[3 * j + 1], norms[3 * j + 2]));
+                this.arrays.texture_coord.push(vec(textures[2 * j], textures[2 * j + 1]));
+            }
+            this.indices = unpacked.indices;
+        }
+        this.normalize_positions(false);
+        this.ready = true;
+    }
+
+    draw(context, program_state, model_transform, material) {               // draw(): Same as always for shapes, but cancel all
+        // attempts to draw the shape before it loads:
+        if (this.ready)
+            super.draw(context, program_state, model_transform, material);
+    }
+}
+
+window.Project_Scene = window.classes.Project_Scene =
+    class Project_Scene extends Scene_Component {
         constructor(context, control_box)
         {
             // The scene begins by requesting the camera, shapes, and materials it will need.
@@ -19,18 +121,16 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
                 torus2: new (Torus.prototype.make_flat_shaded_version())(50, 50),
                 planet_1: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
                 planet_2: new Subdivision_Sphere(3),
+                // person: new Shape_From_File("assets/Character.obj"),
             };
             this.submit_shapes(context, shapes);
 
-            // Make some Material objects available to you:
             this.materials =
                 {
-                    ring: context.get_instance(Ring_Shader).material({diffusivity:1}, {specularity:1}),
                     sun:  context.get_instance(Phong_Shader).material(Color.of(1,0,1,1), {ambient:1}),
                     planet1: context.get_instance(Phong_Shader).material(Color.of(0.76, 0.8, 0.85,1), {ambient:0}, {diffusivity:1}, {specularity:0}),
                     planet2: context.get_instance(Phong_Shader).material(Color.of(0.25, 0.4, 0.15,1), {ambient:0}, {diffusivity:.1}, {specularity:1}),
-                    // TODO:  Fill in as many additional material objects as needed in this key/value table.
-                    //        (Requirement 1)
+
                 };
 
             this.lights = [new Light(Vec.of(5, -10, 5, 1), Color.of(0, 1, 1, 1), 1000)];
@@ -43,5 +143,7 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
 
 
         display(graphics_state) {
+            let model_transform = Mat4.identity();
+            //this.shapes.planet_1.draw(graphics_state, model_transform, this.materials.planet1);
         }
     };
