51bbdb2e69e8214fbbc30bf5b69ba1e548007eb8
Kent Zhang <kent@Kents-MacBook-Pro.local>
2020-03-01 16:05:52 -0800

    Fetched more dependencies

--- main-scene.js
+++ main-scene.js
@@ -1,149 +1,29 @@
-export class Shape_From_File extends Shape {
-    // **Shape_From_File** is a versatile standalone Shape that imports
-    // all its arrays' data from an .obj 3D model file.
-    constructor(filename) {
-        super("position", "normal", "texture_coord");
-        // Begin downloading the mesh. Once that completes, return
-        // control to our parse_into_mesh function.
-        this.load_file(filename);
-    }
+import {tiny, defs} from './examples/common.js';
+                                                  // Pull these names into this module's scope for convenience:
+const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
+        Canvas_Widget, Code_Widget, Text_Widget } = tiny;
 
-    load_file(filename) {                             // Request the external file and wait for it to load.
-        // Failure mode:  Loads an empty shape.
-        return fetch(filename)
-            .then(response => {
-                if (response.ok) return Promise.resolve(response.text())
-                else return Promise.reject(response.status)
-            })
-            .then(obj_file_contents => this.parse_into_mesh(obj_file_contents))
-            .catch(error => {
-                this.copy_onto_graphics_card(this.gl);
-            })
-    }
+    // Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
+    // This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
 
-    parse_into_mesh(data) {                           // Adapted from the "webgl-obj-loader.js" library found online:
-        var verts = [], vertNormals = [], textures = [], unpacked = {};
+    // ******************** Extra step only for when executing on a local machine:  
+    //                      Load any more files in your directory and copy them into "defs."
+    //                      (On the web, a server should instead just pack all these as well 
+    //                      as common.js into one file for you, such as "dependencies.js")
 
-        unpacked.verts = [];
-        unpacked.norms = [];
-        unpacked.textures = [];
-        unpacked.hashindices = {};
-        unpacked.indices = [];
-        unpacked.index = 0;
+const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
+import { Obj_File_Demo }
+  from "./examples/obj-file-demo.js"
 
-        var lines = data.split('\n');
+Object.assign( defs,
+                     { Obj_File_Demo },
+    );
 
-        var VERTEX_RE = /^v\s/;
-        var NORMAL_RE = /^vn\s/;
-        var TEXTURE_RE = /^vt\s/;
-        var FACE_RE = /^f\s/;
-        var WHITESPACE_RE = /\s+/;
+    // ******************** End extra step
 
-        for (var i = 0; i < lines.length; i++) {
-            var line = lines[i].trim();
-            var elements = line.split(WHITESPACE_RE);
-            elements.shift();
+// (Can define Main_Scene's class here)
 
-            if (VERTEX_RE.test(line)) verts.push.apply(verts, elements);
-            else if (NORMAL_RE.test(line)) vertNormals.push.apply(vertNormals, elements);
-            else if (TEXTURE_RE.test(line)) textures.push.apply(textures, elements);
-            else if (FACE_RE.test(line)) {
-                var quad = false;
-                for (var j = 0, eleLen = elements.length; j < eleLen; j++) {
-                    if (j === 3 && !quad) {
-                        j = 2;
-                        quad = true;
-                    }
-                    if (elements[j] in unpacked.hashindices)
-                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
-                    else {
-                        var vertex = elements[j].split('/');
+const Main_Scene = Obj_File_Demo;
+const Additional_Scenes = [];
 
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
-
-                        if (textures.length) {
-                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 0]);
-                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 1]);
-                        }
-
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 0]);
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 1]);
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 2]);
-
-                        unpacked.hashindices[elements[j]] = unpacked.index;
-                        unpacked.indices.push(unpacked.index);
-                        unpacked.index += 1;
-                    }
-                    if (j === 3 && quad) unpacked.indices.push(unpacked.hashindices[elements[0]]);
-                }
-            }
-        }
-        {
-            const {verts, norms, textures} = unpacked;
-            for (var j = 0; j < verts.length / 3; j++) {
-                this.arrays.position.push(vec3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
-                this.arrays.normal.push(vec3(norms[3 * j], norms[3 * j + 1], norms[3 * j + 2]));
-                this.arrays.texture_coord.push(vec(textures[2 * j], textures[2 * j + 1]));
-            }
-            this.indices = unpacked.indices;
-        }
-        this.normalize_positions(false);
-        this.ready = true;
-    }
-
-    draw(context, program_state, model_transform, material) {               // draw(): Same as always for shapes, but cancel all
-        // attempts to draw the shape before it loads:
-        if (this.ready)
-            super.draw(context, program_state, model_transform, material);
-    }
-}
-
-window.Project_Scene = window.classes.Project_Scene =
-    class Project_Scene extends Scene_Component {
-        constructor(context, control_box)
-        {
-            // The scene begins by requesting the camera, shapes, and materials it will need.
-            super(context, control_box);
-            // First, include a secondary Scene that provides movement controls:
-            if (!context.globals.has_controls)
-                context.register_scene_component(new Movement_Controls(context, control_box.parentElement.insertCell()));
-
-            context.globals.graphics_state.camera_transform = Mat4.look_at(Vec.of(0, 10, 20), Vec.of(0, 0, 0), Vec.of(0, 1, 0));
-            this.initial_camera_location = Mat4.inverse(context.globals.graphics_state.camera_transform);
-
-            const r = context.width / context.height;
-            context.globals.graphics_state.projection_transform = Mat4.perspective(Math.PI / 4, r, .1, 1000);
-
-            const shapes = {
-                torus: new Torus(15, 15),
-                torus2: new (Torus.prototype.make_flat_shaded_version())(50, 50),
-                planet_1: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
-                planet_2: new Subdivision_Sphere(3),
-                // person: new Shape_From_File("assets/Character.obj"),
-            };
-            this.submit_shapes(context, shapes);
-
-            this.materials =
-                {
-                    sun:  context.get_instance(Phong_Shader).material(Color.of(1,0,1,1), {ambient:1}),
-                    planet1: context.get_instance(Phong_Shader).material(Color.of(0.76, 0.8, 0.85,1), {ambient:0}, {diffusivity:1}, {specularity:0}),
-                    planet2: context.get_instance(Phong_Shader).material(Color.of(0.25, 0.4, 0.15,1), {ambient:0}, {diffusivity:.1}, {specularity:1}),
-
-                };
-
-            this.lights = [new Light(Vec.of(5, -10, 5, 1), Color.of(0, 1, 1, 1), 1000)];
-        }
-
-        make_control_panel() {
-            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-            this.key_triggered_button("View solar system", ["0"], () => this.attached = () => this.initial_camera_location);
-        }
-
-
-        display(graphics_state) {
-            let model_transform = Mat4.identity();
-            //this.shapes.planet_1.draw(graphics_state, model_transform, this.materials.planet1);
-        }
-    };
+export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
