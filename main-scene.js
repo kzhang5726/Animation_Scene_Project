a8f9f90a59532b3cd8ac7290888aeb1ccb5025ab
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2020-03-01 23:51:22 -0800

    Arrow implementation
    Set to stop at the back wall(when it reaches the same -z coordinate) to mimic collision
    Arrow has parabolic trajectory
    Button for launching the arrow(it only successfully launches AFTER the arrow is reset, which is like 3-4 seconds after landing)

--- main-scene.js
+++ main-scene.js
@@ -1,7 +1,6 @@
 window.Project_Scene = window.classes.Project_Scene =
     class Project_Scene extends Scene_Component {
-        constructor(context, control_box)
-        {
+        constructor(context, control_box) {
             // The scene begins by requesting the camera, shapes, and materials it will need.
             super(context, control_box);
             // First, include a secondary Scene that provides movement controls:
@@ -17,104 +16,164 @@ window.Project_Scene = window.classes.Project_Scene =
             const shapes = {
                 plane: new Square(),
                 floor: new Square(),
-                target : new Cube(),
+                target: new Cube(),
                 person: new Shape_From_File("assets/Character.obj"),
                 arrow: new Shape_From_File("assets/Arrow.obj"),
                 crossbow: new Shape_From_File("assets/Merciless_Crossbow.obj"),
             };
-            shapes.plane.texture_coords = shapes.plane.texture_coords.map(v => Vec.of(v[0]*5, v[1]));
+            shapes.plane.texture_coords = shapes.plane.texture_coords.map(v => Vec.of(v[0] * 5, v[1]));
             shapes.floor.texture_coords = shapes.floor.texture_coords.map(v => Vec.of(v[0], v[1]));
             this.submit_shapes(context, shapes);
 
             this.materials =
-            {
-                floor: context.get_instance(Phong_Shader).material(Color.of(0,0,0,1),
-                    {ambient:1, texture: context.get_instance("assets/concrete.jpg", true)}),
-                back_wall: context.get_instance(Phong_Shader).material(Color.of(0,0,0,1),
-                    {ambient:1, texture: context.get_instance("assets/wood1.jpg", true)}),
-                side_walls: context.get_instance(Phong_Shader).material(Color.of(0,0,0,1),
-                    {ambient:1, texture: context.get_instance("assets/wood2.jpg", true)}),
-                target: context.get_instance(Phong_Shader).material(Color.of(0,0,0,1),
-                    {ambient:1, texture: context.get_instance("assets/target.png", true)}),
-                crossbow: context.get_instance(Phong_Shader).material(Color.of(0,0,0,1),
-                    {ambient:1, texture: context.get_instance("assets/Merciless_Crossbow_default_01.jpg", true)}),
-                red:            context.get_instance( Phong_Shader ).material( Color.of( 1 ,0, 0 ,1 ), { ambient: 1 , specularity: 1} ),
-                green:          context.get_instance( Phong_Shader ).material( Color.of( 0 ,1, 0 ,1 ), { ambient: 1 } ),
-                white:          context.get_instance( Phong_Shader ).material( Color.of( 1 ,1, 1 ,1 ), { ambient: 1 } ),
-                brown:          context.get_instance( Phong_Shader ).material( Color.of( 205/256 ,133/256, 63/256 ,1 ), { ambient: 1 } ),
-            };
+                {
+                    floor: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1),
+                        {ambient: 1, texture: context.get_instance("assets/concrete.jpg", true)}),
+                    back_wall: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1),
+                        {ambient: 1, texture: context.get_instance("assets/wood1.jpg", true)}),
+                    side_walls: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1),
+                        {ambient: 1, texture: context.get_instance("assets/wood2.jpg", true)}),
+                    target: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1),
+                        {ambient: 1, texture: context.get_instance("assets/target.png", true)}),
+                    crossbow: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1),
+                        {ambient: 1, texture: context.get_instance("assets/Merciless_Crossbow_default_01.jpg", true)}),
+                    red: context.get_instance(Phong_Shader).material(Color.of(1, 0, 0, 1), {
+                        ambient: 1,
+                        specularity: 1
+                    }),
+                    green: context.get_instance(Phong_Shader).material(Color.of(0, 1, 0, 1), {ambient: 1}),
+                    white: context.get_instance(Phong_Shader).material(Color.of(1, 1, 1, 1), {ambient: 1}),
+                    brown: context.get_instance(Phong_Shader).material(Color.of(205 / 256, 133 / 256, 63 / 256, 1), {ambient: 1}),
+                };
 
             this.lights = [new Light(Vec.of(5, -10, 5, 1), Color.of(0, 1, 1, 1), 1000)];
+            this.launch = false;
+            this.flying = false;
+            this.launchTime = 0;
+            this.zeroHeightTime = 1000000000; //billion
         }
 
         make_control_panel() {
             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-            this.key_triggered_button("View solar system", ["0"], () => this.attached = () => this.initial_camera_location);
+            this.key_triggered_button("Launch Arrow", ["q"], () => {
+                if (!this.flying) {
+                    this.launch = true;
+                }
+            });
+
         }
 
 
         draw_floor(graphics_state, model_transform) {
-            model_transform = model_transform.times(Mat4.translation([0,-10,-20]));
-            model_transform = model_transform.times(Mat4.rotation(Math.PI/2.5, [1,0,0]));
-            model_transform = model_transform.times(Mat4.scale([40,40,40]));
+            model_transform = model_transform.times(Mat4.translation([0, -10, -20]));
+            model_transform = model_transform.times(Mat4.rotation(Math.PI / 2.5, [1, 0, 0]));
+            model_transform = model_transform.times(Mat4.scale([40, 40, 40]));
             this.shapes.floor.draw(graphics_state, model_transform, this.materials.floor);
         }
 
-        draw_wall_1(graphics_state, model_transform){
-            model_transform = model_transform.times(Mat4.translation([-40,3,-13]));
-            model_transform = model_transform.times(Mat4.rotation( -.25, [1,0,0]));
-            model_transform = model_transform.times(Mat4.rotation(Math.PI/2, [0,1,0]));
-            model_transform = model_transform.times(Mat4.scale([50,50,50]));
+        draw_wall_1(graphics_state, model_transform) {
+            model_transform = model_transform.times(Mat4.translation([-40, 3, -13]));
+            model_transform = model_transform.times(Mat4.rotation(-.25, [1, 0, 0]));
+            model_transform = model_transform.times(Mat4.rotation(Math.PI / 2, [0, 1, 0]));
+            model_transform = model_transform.times(Mat4.scale([50, 50, 50]));
             this.shapes.plane.draw(graphics_state, model_transform, this.materials.side_walls);
         }
 
-        draw_wall_2(graphics_state, model_transform){
-            model_transform = model_transform.times(Mat4.translation([40,3,-13]));
-            model_transform = model_transform.times(Mat4.rotation( -.25, [1,0,0]));
-            model_transform = model_transform.times(Mat4.rotation(-Math.PI/2, [0,1,0]));
-            model_transform = model_transform.times(Mat4.scale([50,50,50]));
+        draw_wall_2(graphics_state, model_transform) {
+            model_transform = model_transform.times(Mat4.translation([40, 3, -13]));
+            model_transform = model_transform.times(Mat4.rotation(-.25, [1, 0, 0]));
+            model_transform = model_transform.times(Mat4.rotation(-Math.PI / 2, [0, 1, 0]));
+            model_transform = model_transform.times(Mat4.scale([50, 50, 50]));
             this.shapes.plane.draw(graphics_state, model_transform, this.materials.side_walls);
         }
 
-        draw_wall_3(graphics_state, model_transform){
-            model_transform = model_transform.times(Mat4.translation([0,0,-70]));
-            model_transform = model_transform.times(Mat4.rotation( -.3, [1,0,0]));
-            model_transform = model_transform.times(Mat4.scale([50,50,50]));
+        draw_wall_3(graphics_state, model_transform) {
+            model_transform = model_transform.times(Mat4.translation([0, 0, -70]));
+            model_transform = model_transform.times(Mat4.rotation(-.3, [1, 0, 0]));
+            model_transform = model_transform.times(Mat4.scale([50, 50, 50]));
             this.shapes.plane.draw(graphics_state, model_transform, this.materials.back_wall);
         }
 
-        draw_room(graphics_state, model_transform){
+        draw_room(graphics_state, model_transform) {
             this.draw_floor(graphics_state, model_transform);
             this.draw_wall_1(graphics_state, model_transform);
             this.draw_wall_2(graphics_state, model_transform);
             this.draw_wall_3(graphics_state, model_transform);
         }
 
-        draw_test_object(graphics_state, model_transform){
-            model_transform = model_transform.times(Mat4.translation([0,-1,0]));
+        draw_test_object(graphics_state, model_transform) {
+            model_transform = model_transform.times(Mat4.translation([0, -1, 0]));
             this.shapes.person.draw(graphics_state, model_transform, this.materials.red);
         }
 
-        draw_target(graphics_state, model_transform, i){
-            model_transform = model_transform.times(Mat4.translation([-30+ i*30,-10,-60]));
-            model_transform = model_transform.times(Mat4.rotation( -.37, [1,0,0]));
-            model_transform = model_transform.times(Mat4.scale([10,10,.1]));
+        draw_target(graphics_state, model_transform, i) {
+            model_transform = model_transform.times(Mat4.translation([-30 + i * 30, -10, -60]));
+            model_transform = model_transform.times(Mat4.rotation(-.37, [1, 0, 0]));
+            model_transform = model_transform.times(Mat4.scale([10, 10, .1]));
             this.shapes.target.draw(graphics_state, model_transform, this.materials.target);
         }
 
-        draw_targets(graphics_state, model_transform){
-            for(let i = 0; i < 3; i++){
+        draw_targets(graphics_state, model_transform) {
+            for (let i = 0; i < 3; i++) {
                 this.draw_target(graphics_state, model_transform, i);
             }
         }
 
-        draw_crossbow(graphics_state, model_transform){
+        draw_crossbow(graphics_state, model_transform) {
 
             this.shapes.crossbow.draw(graphics_state, model_transform, this.materials.crossbow);
         }
 
-        draw_arrow(graphics_state, model_transform){
-            this.shapes.arrow.draw(graphics_state, model_transform, this.materials.brown);
+        draw_arrow(graphics_state, model_transform) {
+            let travelTime = (graphics_state.animation_time - this.launchTime) / 25;
+            let travelCap = travelTime;
+
+            // use travelTime as the slowed down version of time; it's always between 0 & the current amount of time the arrow flies
+            // use travelCap as a coordinate function z(t) that stops when z= 63, the distance to the targets
+            // figure out a better way to sense contact with the targets
+            if (travelCap > 63) {
+                travelCap = 63;
+            }
+
+            let parabola = Math.cos(Math.PI * (travelCap/63)); // we want parabola to be 1 when travelCap is 0, and -1 when travelCap hits the cap
+
+
+            if (this.launch) {
+                this.flying = true;
+                this.launch = false;
+                this.launchTime = graphics_state.animation_time;
+            } else if (this.flying) {
+                // make arrow fly
+
+                // bound the flight, don't have >1 period
+                if (parabola == -1){
+                    // store the time(only if it hasn't been changed before)
+                    if(this.zeroHeightTime == 0) this.zeroHeightTime = travelTime;
+                }
+
+                if(travelTime > this.zeroHeightTime) {
+                    // we have already reached half a period or the "ground"
+                    model_transform = model_transform.times(Mat4.translation([0, -travelCap / 3, -travelCap]));
+                } else{
+                    // we are still a projectile
+                    model_transform = model_transform.times(Mat4.translation([0, parabola * travelCap/3, -travelCap]));
+                }
+
+                model_transform = model_transform.times(Mat4.rotation(-Math.PI * 0.6, [1, 0, 0]));
+                model_transform = model_transform.times(Mat4.scale([5,5,5]));
+                this.shapes.arrow.draw(graphics_state, model_transform, this.materials.red);
+
+                // if 5 seconds passed after we hit the travel cap(when the arrow hits the target), then let the player launch another arrow, so reset variables
+                if (travelTime - travelCap > 40) {
+                    this.flying = false;
+                    this.zeroHeightTime = 1000000000;
+                }
+            } else {
+                // the arrow is still loaded
+                model_transform = model_transform.times(Mat4.rotation(-Math.PI * 0.5, [1,0,0]));
+                model_transform = model_transform.times(Mat4.scale([5,5,5]));
+                this.shapes.arrow.draw(graphics_state, model_transform, this.materials.red);
+            }
         }
 
         display(graphics_state) {
